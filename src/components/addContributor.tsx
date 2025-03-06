6c215ee6d0e667f6f9149ad2125d442ddc0f5001
kalecream <sabrinamedwinter@gmail.com>
2025-03-06 15:41:36 -0500

    Update addContributor.tsx

--- src/components/addContributor.tsx
+++ src/components/addContributor.tsx
@@ -1,68 +1,74 @@
 import React, { Dispatch, SetStateAction } from "react";
 import { FormDetails } from "../types/purchaser";
 
-
-
-
-
 const AddPerson = (
     formData: FormDetails,
     setFormData: Dispatch<SetStateAction<FormDetails>>
-  ) => {
+) => {
     let contributors = formData.contributors;
     contributors++;
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-};
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
 
     setFormData({
-      ...formData,
-      contributors: contributors,
-      persons: [
-        ...formData.persons,
-        {
-          salary: 0,
-          birthYear: 0,
-            downPayment: 0,
-          amountToBorrow: 0,
-            nht: {
+        ...formData,
+        contributors: contributors,
+        persons: [
+            ...formData.persons,
+            {
+              salary: 0,
+              birthYear: 0,
+              downPayment: 0,
+              amountToBorrow: 0,
+              nht: {
                 loan: false,
                 interest: 0,
+                loanAmount: 0,
                 loanTerm: 0,
                 loanMonthly: 0,
                 amountToBorrowAfterNHT: 0,
-          },
-          bankLoan: 0,
-          bankInterest: 0,
-          bankYears: 0,
-        },
-      ],
+                totalRepayment: 0,
+                totalInterest: 0,
+              },
+              bank: {
+                loan: false,
+                totalInterest: 0,
+                loanTerm: 0,
+                interest: 0,
+                loanAmount: 0,
+                loanMonthly: 0,
+              },
+              maxMortgage: 0,
+              monthlyPayment: 0
+            },
+        ],
     });
 
     return (
-      <div className="person">
-        <input
-          type="text"
-          name={`salary${contributors}`}
-          id="salary1"
-          value={formData.persons[formData.contributors].salary}
-          min={1}
-          max={99999999999}
-          onChange={onChange}
-        />
-        <label htmlFor="salary">Your Monthly Income</label>
-        <input
-          type="number"
-          name="birthYear1"
-          id="birthYear1"
-          value={formData.persons[0].birthYear}
-          onChange={onChange}
-        />
-        <label htmlFor="birthYear">Your Birth Year</label>
-      </div>
+        <div className="person">
+            <input
+                type="text"
+                name={`salary${contributors}`}
+                id="salary1"
+                value={formData.persons[formData.contributors].salary}
+                min={1}
+                max={99999999999}
+                onChange={onChange}
+            />
+            <label htmlFor="salary">Your Monthly Income</label>
+            <input
+                type="number"
+                name="birthYear1"
+                id="birthYear1"
+                value={formData.persons[0].birthYear}
+                onChange={onChange}
+            />
+            <label htmlFor="birthYear">Your Birth Year</label>
+        </div>
     );
-  };
+};
 
-  export default AddPerson;
+export default AddPerson;
