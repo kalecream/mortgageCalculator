1b8484662248dcc2edc7b31330d7366005ae383b
kalecream <sabrinamedwinter@gmail.com>
2025-03-06 15:37:36 -0500

    Update purchaser.d.ts

--- src/types/purchaser.d.ts
+++ src/types/purchaser.d.ts
@@ -1,61 +1,54 @@
-
-
 interface NHTDetails {
-  loan: boolean;
-  interest: number;
-  loanAmount: number;
-  loanTerm: number;
-  loanMonthly: number;
-  loanYearly?: number;
-  amountToBorrowAfterNHT: number;
-  totalRepayment?: number;
-  totalInterest?: number;
-}
-
-interface BankDetails {
-  loan: boolean;
-  interest: number;
-  loanAmount: number;
-  loanTerm: number;
-  loanMonthly: number;
-  loanYearly?: number;
-  totalRepayment?: number;
-  totalInterest?: number;
+    loan: boolean;
+    interest: number;
+    loanAmount: number;
+    loanTerm: number;
+    loanMonthly: number;
+    loanYearly?: number;
+    amountToBorrowAfterNHT: number;
+    totalRepayment: number;
+    totalInterest: number;
 }
 
 interface HousingCosts {
-  cost: number;
-  totalDeposit: number;
-  transferTax: number;
-  stampduty: number;
-  legalFees: number;
-  legalFeesRate: number;
-  salesAgreement: number;
-  registrationFee: number;
-  realEstateAgent: number;
+    cost: number;
+    totalDeposit: number;
+    transferTax: number;
+    stampduty: number;
+    legalFees: number;
+    legalFeesRate: number;
+    salesAgreement: number;
+    registrationFee: number;
+    realEstateAgent: number;
 }
 
 interface FormDetails {
-  contributors: number;
-  house: HousingCosts;
-  persons: ContributorDetails[];
+    contributors: number;
+    house: HousingCosts;
+    persons: ContributorDetails[];
 }
 
 interface ContributorDetails {
-  name?: string;
-  salary: number;
-  birthYear: number;
-  downPayment: number;
-  amountToBorrow: number;
-  maxMortgage: number;
-  monthlyPayment: number;
-  nht: NHTDetails;
-  bank: BankDetails;
+    name?: string;
+    salary: number;
+    birthYear: number;
+    downPayment: number;
+    amountToBorrow: number;
+    maxMortgage: number;
+    monthlyPayment: number;
+    nht: NHTDetails;
+    bank: BankDetails;
 }
 
-export type {
-  FormDetails,
-  ContributorDetails,
-  NHTDetails,
-  HousingCosts
+interface BankDetails {
+    loan: boolean;
+    interest: number;
+    loanAmount: number;
+    loanTerm: number;
+    loanMonthly: number;
+    loanYearly?: number;
+    totalRepayment?: number;
+    totalInterest?: number;
 }
+
+export type { FormDetails, ContributorDetails, NHTDetails, HousingCosts };
