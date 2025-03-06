9fca62cc87cb63311f938b30ad756db911e75a5f
kalecream <sabrinamedwinter@gmail.com>
2025-03-06 15:38:43 -0500

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,21 +1,20 @@
+import DownloadPDF from "./DownloadPDF";
+
 export const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="button-container">
-        <button>Download PDF</button>
-      <button>Settings</button>
-      </div>
-      <p>
-        Based on the spreadsheet found at{" "}
-        <a href="https://financialcentsibility.com/calculators/">
-          Financial Centsibility
-        </a>
-      </p>
-      <p>
-        Made with ☕ by{" "}
-        <a href="https://www.sabrinamedwinter.com/">Sabrina Medwinter</a>
-      </p>
-      <p> Disclaimer: This calculator is not afflicated with any of the named parties on this page. </p>
-    </footer>
-  );
+    return (
+        <footer className="footer">
+            <h1>Mortgage Calculator</h1>
+            <div className="button-container">
+                <DownloadPDF />
+            </div>
+            <p>
+                Made by{" "}
+                <a href="https://www.yunghigue.com/">Yung Higue</a> <br /> based
+                on a spreadsheet from{" "}
+                <a href="https://financialcentsibility.com/calculators/">
+                    Financial Centsibility.
+                </a>
+            </p>
+        </footer>
+    );
 };
