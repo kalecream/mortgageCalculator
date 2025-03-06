51e1ab6a90bb08ff8e61800816f1cd6e7cb89e60
kalecream <sabrinamedwinter@gmail.com>
2025-03-06 15:37:16 -0500

    feat: Add download ability

---
+++ src/components/DownloadPDF.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
+
+interface DownloadPDFProps {
+    fileName?: string;
+}
+
+const DownloadPDF: React.FC<DownloadPDFProps> = ({
+    fileName = `MortgageCalculator.pdf`,
+}) => {
+    const handleDownloadPDF = async () => {
+        try {
+            const element = document.getElementById('root');
+
+            if (!element) {
+                return;
+            }
+
+            const canvas = await html2canvas(element, {
+                scale: 2,
+                useCORS: true,
+            });
+
+            const imgData = canvas.toDataURL("image/png");
+            const pdf = new jsPDF("p", "mm", "a4"); 
+            const imgWidth = 210;
+            const imgHeight = (canvas.height * imgWidth) / canvas.width; 
+            pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+            pdf.save(fileName);
+        } catch (error) {
+            console.error("Error generating PDF:", error);
+        }
+    };
+
+    return (
+        <button
+            onClick={handleDownloadPDF}
+        >
+            Download as PDF
+        </button>
+    );
+};
+
+export default DownloadPDF;
