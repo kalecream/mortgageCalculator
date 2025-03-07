d47cd1717d11456cbc692dd835961299a7e43513
kalecream <sabrinamedwinter@gmail.com>
2025-03-06 20:52:17 -0500

    chore: remove break

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
             <DownloadPDF />
             <p>
                 Made by <a href="https://www.yunghigue.com/">Yung Higue</a>{" "}
-                <br /> based on a spreadsheet from{" "}
+                based on a spreadsheet from{" "}
                 <a href="https://financialcentsibility.com/calculators/">
                     Financial Centsibility.
                 </a>
